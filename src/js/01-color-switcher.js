8b71010f422c6e43828558c05d3ef0cb4a6af279
MaksVibe <parunovmaksym@gmail.com>
2021-11-17 18:14:54 +0200

    1st Task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,9 +1,16 @@
-const startBtn = document.querySelector("data-start");
-const stopBtn = document.querySelector("data-stop");
-let timer = null;
+const startBtn = document.querySelector("[data-start]");
+const stopBtn = document.querySelector("[data-stop]");
+
+let timer;
 
 startBtn.addEventListener("click", () => {
   timer = setInterval(() => {
-    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    let bgColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    document.querySelector("body").style.backgroundColor = bgColor;
+    return;
   }, 500);
+});
+
+stopBtn.addEventListener("click", () => {
+  clearInterval(timer);
 });
