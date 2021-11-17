1c57e7e0305f750550da4eee954a6dc0001f0f26
MaksVibe <parunovmaksym@gmail.com>
2021-11-17 17:52:27 +0200

    Test update

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,9 @@
+const startBtn = document.querySelector("data-start");
+const stopBtn = document.querySelector("data-stop");
+let timer = null;
 
+startBtn.addEventListener("click", () => {
+  timer = setInterval(() => {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }, 500);
+});
