6f95511688a75a46f0e61824e0c0e472d0410d87
MaksVibe <parunovmaksym@gmail.com>
2021-11-19 00:12:54 +0200

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,50 +2,92 @@ import flatpickr from "flatpickr";
 import Notiflix from "notiflix";
 import "flatpickr/dist/flatpickr.min.css";
 
-const timer = document.querySelector(".timer");
-const fields = document.querySelector(".timer").children;
+const Refs = {
+  startBtn: document.querySelector("[data-start]"),
+  day: document.querySelector("[data-days]"),
+  hour: document.querySelector("[data-hours]"),
+  minute: document.querySelector("[data-minutes]"),
+  second: document.querySelector("[data-seconds]"),
+};
 
 let options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
+
   onClose(selectedDates) {
     if (selectedDates[0] <= options.defaultDate) {
+      Refs.startBtn.setAttribute("disabled", "true");
       Notiflix.Notify.failure("Please choose a date in the future");
+    } else {
+      Refs.startBtn.removeAttribute("disabled");
     }
-    console.log(selectedDates[0]);
+
+    let currentTime = selectedDates[0] - options.defaultDate;
+    updateTimerFace(convertMs(currentTime));
+
+    const timer = {
+      intervalId: null,
+      isActive: false,
+
+      start() {
+        if (this.isActive) {
+          return;
+        }
+
+        this.isActive = true;
+        this.intervalId = setInterval(() => {
+          currentTime -= 1000;
+          updateTimerFace(convertMs(currentTime));
+
+          if (currentTime < 1000) {
+            clearInterval(this.intervalId);
+            this.isActive = false;
+          }
+        }, 1000);
+      },
+    };
+    Refs.startBtn.addEventListener("click", timer.start);
   },
 };
-console.log(options.defaultDate);
+
 flatpickr("#datetime-picker", options);
 
-document.querySelector("[data-start]").addEventListener("click", () => {});
+function pad(value) {
+  return String(value).padStart(2, "0");
+}
 
-function convertMs(ms) {
-  // Number of milliseconds per unit of time
+function updateTimerFace({ days, hours, minutes, seconds }) {
+  Refs.day.textContent = `${days}`;
+  Refs.hour.textContent = `${hours}`;
+  Refs.minute.textContent = `${minutes}`;
+  Refs.second.textContent = `${seconds}`;
+}
+
+function convertMs(time) {
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
-  // Remaining days
-  const days = Math.floor(ms / day);
-  // Remaining hours
-  const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
-  const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  const days = pad(Math.floor(time / day));
+  const hours = pad(Math.floor((time % day) / hour));
+  const minutes = pad(Math.floor(((time % day) % hour) / minute));
+  const seconds = pad(Math.floor((((time % day) % hour) % minute) / second));
 
   return { days, hours, minutes, seconds };
 }
 
 // Stylization
-timer.style.display = "flex";
-timer.style.gap = "30px";
+const styleRefs = {
+  timerFace: document.querySelector(".timer"),
+  fields: document.querySelector(".timer").children,
+};
+styleRefs.timerFace.style.display = "flex";
+styleRefs.timerFace.style.gap = "30px";
 
-for (const field of fields) {
+for (const field of styleRefs.fields) {
   field.style.display = "flex";
   field.style.flexDirection = "column";
   field.style.alignItems = "center";
